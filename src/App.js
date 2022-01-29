a28dee0b129d9fd35c27b31579b772a6039de347
mrtnmmn <mrtn.lmba@gmail.com>
2022-01-29 22:03:05 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [stateButton, setStateButton] = useState('buttons')
 
   let matrix = []
 
@@ -19,14 +22,19 @@ function App() {
 
   console.log(matrix)
 
+  function onClickFunction(index) {
+    console.log(index)
+    setStateButton('buttonClicked')
+  } 
+
   return (
     <div className="App">
       <header className="App-header">
         <div className='divCont'>
-          {matrix.map((fila) => {
+          {matrix.map((fila, indexFila) => {
             return <div>{
-              fila.map((element)=> {
-                return <button className='buttons' onClick={this.className = 'buttonClicked'}>{element}</button>
+              fila.map((element, index)=> {
+                return <button key={indexFila * 10 +index} className={stateButton} onClick={() => onClickFunction(indexFila * 10 + index)}>{element}</button>
               })}
             </div> 
           })}
