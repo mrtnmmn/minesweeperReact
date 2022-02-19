506e8a5f4e65fba385766557f4939105be246b92
mrtnmmn <mrtn.lmba@gmail.com>
2022-02-19 13:16:41 +0100

    uploaded changes from prueba

--- src/App.js
+++ src/App.js
@@ -2,21 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let matrix = []
+
+  for (let i = 0; i < 10; i ++) {
+    let fila = []
+
+    for (let j = 0; j < 10; j++) {
+      fila.push("" + i + j)
+    }
+
+    matrix.push(fila)
+    console.log(fila)
+
+  }
+
+  console.log(matrix)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='divCont'>
+          {matrix.map((fila) => {
+            return <div>{
+              fila.map((element)=> {
+                return <button className='buttons' onClick={this.className = 'buttonClicked'}>{element}</button>
+              })}
+            </div> 
+          })}
+        </div>
       </header>
     </div>
   );
